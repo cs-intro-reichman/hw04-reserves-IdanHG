9ba6e545d7be55072e7040b5034a0e2a6d6db877
Idan Harel Gross <idannhg@Idans-MacBook-Pro.local>
2024-02-10 18:19:47 +0200

    first

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,111 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+
+        /* testing...
+
+        int[] array = new int[] {-202,48,13,7,8};
+        int[] array2 = new int[] {2,1};
+        System.out.println("The missing integer is " + findMissingInt(array));
+        System.out.println("The second max Value is " + secondMaxValue(array));
+        System.out.println(containsTheSameElements(array, array2)); 
+        System.out.println(isSorted(array));
+         */
     }
-    
+
+    /*
+    finds a missing integer in an array that contains every integer between 0
+    and the kength of the array - except fot one.
+     */  
     public static int findMissingInt (int [] array) {
-        // Write your code here:
+        int n = array.length;
+        for (int i = 0; i <= n; i++) {
+            if (!contains(array, i)) return i;
+        }
         return -1;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max = max(array);
+        int min = min(array);
+        int diff = max - min;
+        int checker = 0; //checks if a number is in the array
+        int maxCounter = 0; //says how many timmes maximunm appears maximum appers in the array;
+        //loop to if max appears more than once. if yes - return max.
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == max) maxCounter++;
+            if (maxCounter > 1) return max;
+        }
+        //loop to find second max in case max appears once.
+        for (int i = 1 ; i <= diff; i++) {
+            checker = max - (i);
+            if (contains(array, checker)) return checker;
+        }
+       return -1;
     }
-
+    //checks if to arrays contain the same elements
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        boolean same = true;
+        for (int i = 0; i < array1.length ; i++) {
+            if (!contains(array2, array1[i])) {
+                same = false;
+            }
+        }
+        for (int i = 0; i < array2.length ; i++) {
+            if (!contains(array1, array2[i])) {
+                same = false;
+            }
+        }
+        return same;
     }
-
+    //checks if an array is sorted in an increasing order or a decreasing order;
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        boolean increasing = false;
+        for (int i = 0; i < array.length; i++) { // if array[i+1] == array[i] we will continuew to check 
+            if (array[i+1] > array[i]) {
+                increasing = true; //array is increaing. we will check if sorted increasingly.
+                break;
+            }
+            else if (array[i+1] < array[i]) {
+                increasing = false; //array is decreasing. we will check if sorted decreasingly.
+                break;
+            } 
+        }
+        if (increasing) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (!(array[i+1] >= array[i])) return false;
+            }
+        }
+        else if (!increasing) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (!(array[i+1] <= array[i])) return false;
+        }
+            }    
+        return true;
     }
 
+    //contains - checks if an integer exists in the array
+    public static boolean contains (int[] arr, int num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) return true;
+        }
+        return false; 
+    }
+
+    //return maximum value in an integer array
+    public static int max (int[] arr) {
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        } 
+        return max;
+    }
+
+    //return min value in an integer array
+    public static int min (int[] arr) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            min = Math.min(min, arr[i]);
+        }
+        return min;
+    }
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,84 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
-    }
+        //System.out.println(capVowelsLowRest(args[0]));
+        System.out.println(camelCase(args[0]));
 
+        /*char c =  args[1].charAt(0);
+        int[] arr = (allIndexOf(args[0], c));
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]); 
+        }*/
+        }
+    
+    //changes a string such taht vouwels are Capital and other letters appear as lowercase letters.
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+       String upperVowels = "AEIOU";
+       String lowerVowels = "aeiou";
+       String lowerLetters = "bcdfghjklmnpqrstvwxyz";
+       String upperLetters = "BCDFGHJKLMNPQRSTWXYZ";
+       String res = "";
+       for (int i = 0 ; i < string.length(); i++) {
+            if (upperVowels.indexOf(string.charAt(i)) != -1 || string.charAt(i) == ' ')
+                res += string.charAt(i);
+            else if (lowerVowels.indexOf(string.charAt(i)) != -1) 
+                res += (char) (string.charAt(i) - 32);
+            else if (lowerLetters.indexOf(string.charAt(i)) != -1)
+                res += string.charAt(i);
+            else if (upperLetters.indexOf(string.charAt(i)) != -1)
+                res += (char) (string.charAt(i) + 32);
+       } 
+        return res;
     }
 
+//fix!!
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String res = "";
+        boolean inWord = false;
+        boolean first = true;
+        for (int i = 0; i < string.length(); i++) {
+            
+            if (string.charAt(i) == ' ') { // is space
+                inWord = false;
+                continue;
+            }
+
+            if (!inWord) { // first letter in word
+                res += first ? toLower(string.charAt(i)) : toUpper(string.charAt(i));
+                first = false;
+                inWord = true;
+                continue;
+            }
+            
+            res += toLower(string.charAt(i));
+            
+        }
+        return res;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int counter = 0; //count how many time the letter appears.
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) counter++;
+        }
+        int[] res = new int[counter];
+        counter = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                res[counter] = i;
+                counter++;
+            }
+        }
+        return res;
+    }
+
+    public static char toLower (char c) {
+        if (c >= 'A' && c <= 'Z') return (char) (c + 32);
+        return c;
+    }
+
+    public static char toUpper (char c) {
+        if (c >= 'a' && c <= 'z') return (char) (c - 32);
+        return c;
     }
 }
